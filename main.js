7570035df8528242f1b0cdd967fbf5eb0bbbf49c
Nick Clawson <NickClawson@fullsail.edu>
2012-06-07 20:47:00 -0400

    rearranged order of form elements in a more logical sequence.
    created function to save data to local storage

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ window.addEventListener("DOMContentLoaded", function(){ //This function waits un
 		var formTag = document.getElementsByTagName("form"); 
 		var selectLi = $('select');
 		var makeSelect = document.createElement('select');
-		
 		makeSelect.setAttribute("id", "locations");//sets id attribute of select element to 'location'
-		for(var i = 0, j = rideLocation.length; i<j; i++){
+		
+		for(var i = 0, j = rideLocation.length; i<j; i++){ //for loop cycles through rideLocation array and assigns the value of each index to the new select element
 			var makeOption = document.createElement('option');
 			var optText = rideLocation[i];
 			makeOption.setAttribute("value", optText);
@@ -25,14 +25,42 @@ window.addEventListener("DOMContentLoaded", function(){ //This function waits un
 		selectLi.appendChild(makeSelect);
 
 	};
-	var rideLocation = ["--Choose a Park--", "Cedar Point", "King's Island","Kennywood","Hersheypark"];//These are default variables from the drop menu
-	makeCategory()
+	function getSelectedRadio(){//function determines if radio button is checked
+		var radios = document.forms[0].type;
+		for(var i = 0; i < radios.length; i++){
+			if(radios[i].checked){
+				typeValue = radios[i].value;
+			};
+		};
+	};
+
+	function saveData(){ //function will add form data to local storage
+		getSelectedRadio();
+		var id = Math.floor(Math.random() * 132145433);
+		var storageItem = {};
+			storageItem.ride = ["Ride Name", $("rname").value];
+			storageItem.park = ["Park", $("locations").value];
+			storageItem.type = ["Ride Type", typeValue];
+			storageItem.rating = ["Rating", $("rating").value];
+			storageItem.date = ["Date Ridden", $("date").value];
+			storageItem.comments = ["Comments", $("comments").value];
+		
+		//local storage can only accept strings! below converts above object to string and saves it
+		localStorage.setItem(id, JSON.stringify(storageItem));
+		alert("Rating Saved, Ride On!");
+	};
+
+
+	//Default Variables
+	var rideLocation = ["--Choose a Park--", "Cedar Point", "King's Island","Kennywood","Hersheypark"];
+	var typeValue;
+	makeCategory();
 
 	//The below variales set link and submit button click events
-	var displayLink = $('displayLink');
-	displayLink.addEventListener("click", getData);
-	var clearLink = $("clear");
-	clearLink.addEventListener("click", clearLocal);
+//	var displayLink = $('displayLink');
+//	displayLink.addEventListener("click", getData);
+//	var clearLink = $("clear");
+//	clearLink.addEventListener("click", clearLocal);
 	var save = $("submit");
 	save.addEventListener("click", saveData);
 });
